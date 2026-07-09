c4f2c1703a74559356bcf7d121028897494b0162
Tapshya Mangal <mangaltapasya@gmail.com>
2026-07-09 20:39:08 +0530

    Add pattern8 function to generate a specific pattern

---
+++ Patterns/pattern8.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void pattern8(int n) {
+       
+       for(int i=0 ; i<n;i++) {
+        for(int j=0 ; j<i;j++) {
+            cout<<" ";
+        }
+        for(int j=0 ; j<2*n-(2*i+1) ; j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+       }
+    }
+};
