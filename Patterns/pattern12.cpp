8b6b694b011d098ed8691051f87aca7773a60f94
Tapshya Mangal <mangaltapasya@gmail.com>
2026-07-10 20:20:24 +0530

    Add pattern12 function to print a specific pattern

---
+++ Patterns/pattern12.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void pattern12(int n) {
+         int space = 2*(n-1);
+         for(int i=1 ;i<=n ;i++) {
+            for(int j=1 ;j<=i ;j++) {
+                cout<<j;
+            }
+            for(int j=1 ;j<=space ;j++) {
+                cout<<" ";
+            }
+            for(int j=i ;j>=1 ;j--) {
+                cout<<j;
+            }
+            cout<<endl;
+            space -= 2;
+         }
+    }
+};
